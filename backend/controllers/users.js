fd7874d98f771152ddf6ee35798d41a033a5c68e
Etril <a.croissantguyamier@gmail.com>
2024-12-22 22:39:53 +0100

    Modification de la logique bcrypt

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -8,20 +8,25 @@ const TOKENSECRET=process.env.TOKENSECRET
 /*** Cette route est utilisée pour connecter un utilisateur existant */
 
 exports.login = (req, res, next) => {
-  User.findOne({
-    name: req.body.name,
-  })
+  console.log("Login request received:", req.body); // Log de la requête
+
+  User.findOne({ name: req.body.name })
     .then((user) => {
       if (user === null) {
-        res.status(401).json({ message: "Informations eronnées" });
+        console.log("User not found");
+        return res.status(401).json({ message: "Informations erronées" });
       } else {
+        console.log("User found, comparing password");
+
         bcrypt
           .compare(req.body.password, user.password)
           .then((valid) => {
             if (!valid) {
-              res.status(401).json({ message: "Informations eronnées" });
+              console.log("Password is incorrect");
+              return res.status(401).json({ message: "Informations erronées" });
             } else {
-              res.status(200).json({
+              console.log("Password is correct, generating token");
+              return res.status(200).json({
                 userId: user._id,
                 token: jwt.sign({ userId: user._id }, TOKENSECRET, {
                   expiresIn: "24h",
@@ -29,8 +34,14 @@ exports.login = (req, res, next) => {
               });
             }
           })
-          .catch((error) => res.status(500).json({ error }));
+          .catch((error) => {
+            console.error("Error in bcrypt comparison:", error);
+            return res.status(500).json({ error: error.message });
+          });
       }
     })
-    .catch((error) => res.status(500).json({ error }));
+    .catch((error) => {
+      console.error("Error in finding user:", error);
+      return res.status(500).json({ error: error.message });
+    });
 };
